96d8bd16aac15069f4a3204c3ea6a6a04c73b0b8
silvajeferson82 <jeferson.silva@tiva.app>
2022-10-15 19:59:23 -0300

    feat: define null = false

--- db/migrate/20221015225448_create_tasks.rb
+++ db/migrate/20221015225448_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string :description
+      t.string :description, null: false
       t.date :due_date
-      t.boolean :done
+      t.boolean :done, default: false, null: false
 
       t.timestamps
     end
